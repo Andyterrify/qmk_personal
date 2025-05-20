f3101f96314e7a7c25c4ba1bbb0a7b3095863c4a
Andrei Vasile <andreibvasile@gmail.com>
2025-05-20 23:05:54 +0100

    brought layout back to home row mod keys on the bottom don't know why/where this got changed but not fun

--- keyboards/sofle/rev1/keymaps/andyterrify/keymap.c
+++ keyboards/sofle/rev1/keymaps/andyterrify/keymap.c
@@ -38,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT(
 /* 1 */ KC_GRV,  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,        /* SPLIT */           XXXXXXX, XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,             XXXXXXX, /* # */
 /* 2 */ QK_GESC,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,           /* SPLIT */           KC_Y, KC_U,         KC_I,         KC_O,         KC_P,             KC_BSPC, /* # */
-/* 3 */ KC_TAB,   LCTL_T(KC_A), LSFT_T(KC_S), LGUI_T(KC_D), LALT_T(KC_F), KC_G,           /* SPLIT */           KC_H, RALT_T(KC_J), RGUI_T(KC_K), RSFT_T(KC_L), RCTL_T(KC_SCLN),  KC_QUOT, /* # */
-/* 4 */ KC_BSLS,  KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_MUTE,  /* SPLIT */ KC_MUTE,  KC_N, KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          KC_TILD, /* # */
+/* 3 */ KC_TAB,   KC_A, KC_S, KC_D, KC_F, KC_G,           /* SPLIT */           KC_H,                                             KC_J, KC_K, KC_L, KC_SCLN,  KC_QUOT, /* # */
+/* 4 */ KC_BSLS,  LCTL_T(KC_Z),         LSFT_T(KC_X),         LGUI_T(KC_C),         LALT_T(KC_V),         KC_B,   KC_MUTE,  /* SPLIT */ KC_MUTE,  KC_N, RALT_T(KC_M),         RGUI_T(KC_COMM),      RSFT_T(KC_DOT),       RCTL_T(KC_SLSH),          KC_TILD, /* # */
 /* Special */   QK_BOOT,  XXXXXXX,  OSL(_LOWER), MO(_LOWER), KC_SPC, /* SPLIT */ KC_ENT, MO(_RAISE), OSL(_RAISE),  XXXXXXX,  QK_BOOT
 ),
 
