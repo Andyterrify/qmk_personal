327f0938cf4918f77641462492769dd8c9273cd0
Andrei Vasile <andreibvasile@gmail.com>
2025-05-21 21:47:18 +0100

    split layout top/bottom left/right the info.json file is crucial for this however it only works when under qmk_firmware, not qmk_userspace, so it will need moving manually if current structure is lost

--- keyboards/sofle/rev1/keymaps/andyterrify/keymap.c
+++ keyboards/sofle/rev1/keymaps/andyterrify/keymap.c
@@ -36,12 +36,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 
-[_BASE] = LAYOUT(
-/* 1 */ KC_GRV,  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,        /* SPLIT */           XXXXXXX, XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,             XXXXXXX, /* # */
-/* 2 */ QK_GESC,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,           /* SPLIT */           KC_Y, KC_U,         KC_I,         KC_O,         KC_P,             KC_BSPC, /* # */
-/* 3 */ KC_TAB,   KC_A, KC_S, KC_D, KC_F, KC_G,           /* SPLIT */           KC_H,                                             KC_J, KC_K, KC_L, KC_SCLN,  KC_QUOT, /* # */
-/* 4 */ KC_BSLS,  LCTL_T(KC_Z),         LSFT_T(KC_X),         LGUI_T(KC_C),         LALT_T(KC_V),         KC_B,   KC_MUTE,  /* SPLIT */ KC_MUTE,  KC_N, RALT_T(KC_M),         RGUI_T(KC_COMM),      RSFT_T(KC_DOT),       RCTL_T(KC_SLSH),          KC_TILD, /* # */
-/* Special */   QK_BOOT,  XXXXXXX,  OSL(_LOWER), MO(_LOWER), KC_SPC, /* SPLIT */ KC_ENT, MO(_RAISE), OSL(_RAISE),  XXXXXXX,  QK_BOOT
+[_BASE] = LAYOUT_some(
+  // ,---------------+----------------+----------------+----------------+----------------+----------------.
+          KC_GRV,           XXXXXXX,        XXXXXXX,          XXXXXXX,        XXXXXXX,        XXXXXXX,
+  // |---------------+----------------+----------------+----------------+----------------+----------------|
+          QK_GESC,          KC_Q,           KC_W,             KC_E,           KC_R,           KC_T,
+  // |---------------+----------------+----------------+----------------+----------------+----------------|
+          KC_TAB,           KC_A,           KC_S,             KC_D,           KC_F,           KC_G,     //|-------.
+  // |---------------+----------------+----------------+----------------+----------------+----------------|  MUTE |
+          KC_BSLS,      LCTL_T(KC_Z),    LSFT_T(KC_X),    LGUI_T(KC_C),    LALT_T(KC_V),      KC_B,     //|-------|
+  // `---------------+----------------+----------------+----------------+----------------+----------------/       /
+        QK_BOOT,         XXXXXXX,          OSL(_LOWER),    MO(_LOWER),       KC_SPC,          KC_MUTE,
+  //           `------------------------------------/
+
+
+  // ,---------------+----------------+----------------+----------------+----------------+----------------.
+          XXXXXXX,          XXXXXXX,        XXXXXXX,          XXXXXXX,        XXXXXXX,        XXXXXXX,
+  // |---------------+----------------+----------------+----------------+----------------+----------------|
+          KC_Y,             KC_U,           KC_I,             KC_O,           KC_P,           KC_BSPC,
+  // |---------------+----------------+----------------+----------------+----------------+----------------|
+          KC_H,             KC_J,           KC_K,             KC_L,           KC_SCLN,        KC_QUOT,  //|-------.
+  // |---------------+----------------+----------------+----------------+----------------+----------------|  MUTE |
+          KC_N,        RALT_T(KC_M),   RGUI_T(KC_COMM), RSFT_T(KC_DOT),  RCTL_T(KC_SLSH),     KC_TILD,  //|-------|
+  // `---------------+----------------+----------------+----------------+----------------+----------------/       /
+        KC_MUTE,         KC_ENT,          MO(_RAISE),    OSL(_RAISE),       XXXXXXX,          QK_BOOT
+  //           `------------------------------------/
 ),
 
 
