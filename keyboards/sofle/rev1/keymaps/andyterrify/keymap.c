a6c7ad3c6d852d466e2de699fc5ede443128a467
Andrei Vasile <andreibvasile@gmail.com>
2025-05-24 11:48:49 +0100

    game mode changes

--- keyboards/sofle/rev1/keymaps/andyterrify/keymap.c
+++ keyboards/sofle/rev1/keymaps/andyterrify/keymap.c
@@ -149,14 +149,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_GAME] = LAYOUT_some(
-  QK_GESC,    KC_1,           KC_2,           KC_3,           KC_4,           KC_5,
+  XXXXXXX,    XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
   KC_TAB,     KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,
-  KC_LSFT,     KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
+  KC_LSFT,    KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
   KC_LCTL,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,
-        XXXXXXX, KC_LALT, OSL(_GAME_NUMBER), MO(_RAISE),   KC_SPC, KC_MUTE,
+        XXXXXXX, MO(_RAISE), KC_LALT, OSL(_GAME_NUMBER),   KC_SPC, KC_MUTE,
 
-  KC_6,   KC_7,           KC_8,           KC_9,           KC_0,               KC_BSPC,
-  KC_Y,   KC_U,           KC_I,           KC_O,           KC_P,               XXXXXXX,
+  XXXXXXX,XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,            XXXXXXX,
+  KC_Y,   KC_U,           KC_I,           KC_O,           KC_P,               KC_BSPC,
   KC_H,   KC_J,           KC_K,           KC_L,           KC_SCLN,            KC_QUOT,
   KC_N,   KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,            KC_HASH,
         XXXXXXX, KC_ENT, MO(_RAISE), XXXXXXX, XXXXXXX, TG(_GAME)
@@ -164,13 +164,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_GAME_NUMBER] = LAYOUT_some(
   _______,    _______,    _______,    _______,    _______,    _______,
-  _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,
-  _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,
+  QK_GESC,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,
+  KC_TILD,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,
   _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,
         XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX,   KC_SPC, KC_MUTE,
 
   _______,    _______,    _______,    _______,    _______,    _______,
-  KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,
+  KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,       _______,
   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,
   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,
         XXXXXXX, KC_ENT, MO(_RAISE), XXXXXXX, XXXXXXX, XXXXXXX
